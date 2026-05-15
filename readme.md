57daf9d9a16d030b82f56e9734e2a22db4ac81de
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-14 23:23:26 -0400

    Added the logic fo fetch inly texxt field data

---
+++ readme.md
@@ -0,0 +1,13 @@
+Task 3 o/p
+
+
+```
+{
+  "id": "adf7b27fb90c602ef9d0ca6f0d76348c",
+  "text": "Montpelier, Vermont is the only state capital without a McDonalds.",
+  "source": "djtech.net",
+  "source_url": "https://www.djtech.net/humor/shorty_useless_facts.htm",
+  "language": "en",
+  "permalink": "https://uselessfacts.jsph.pl/api/v2/facts/adf7b27fb90c602ef9d0ca6f0d76348c"
+}
+```
--- server.js
+++ server.js
@@ -3,26 +3,41 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// app.get('/', (res,req) => {
-// res.send('hello');
-// });
 
-app.get('/api/fun-fact', async(req, res) => {
+// Fetch data from external API
+app.get('/api/fun-fact', async (req, res) => {
     try {
         const response = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random');
         res.json(response.data);
-    } catch(error) {
+    } catch (error) {
         if (error.response) {
             console.error('API Error:', error.response.status, error.response.data);
             res.status(error.response.status).json({ message: 'Error fetching data from external API.' });
         } else {
             console.error('Network Error:', error.message);
-            res.status(500).json({message: 'A network error occurred.'});
+            res.status(500).json({ message: 'A network error occurred.' });
         }
     }
-});  
+});
+// Extract only the fact text
+app.get("/api/fun-fact/text", async (req, res) => {
 
+    try {
+        const response = await axios.get(
+            "https://uselessfacts.jsph.pl/api/v2/facts/random"
+        );
+        // Extract only text
+        const factText = response.data.text;
+        res.json({
+            fact: factText
+        });
+    } catch (error) {
 
+        res.status(500).json({
+            error: "Could not fetch fact text"
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running at port:${port}`);
